6eb935fd541916d89413e5d2c26b5eb0e2b29939
Ronaldo Silva Nascimento <silvaronaldo7662@gmail.com>
2026-04-10 10:45:23 -0300

    funcionalidades dos componentes

--- src/app/task-details/task-details.ts
+++ src/app/task-details/task-details.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TaskModel } from '../models/task';
+import { Task } from '../services/task';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-details',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-details.html',
   styleUrl: './task-details.css',
 })
 export class TaskDetails {
 
+  task?: TaskModel;
+
+  constructor(private router: Router, private route: ActivatedRoute, private taskService: Task) {}
+
+  ngOnInit() {
+
+    let id = this.route.snapshot.paramMap.get('id');
+
+    if(id === null) {
+      this.navigateBack();
+    }
+    else {
+      this.task = this.taskService.getById(+id);
+
+      if(this.task === undefined) {
+        this.navigateBack();
+      }
+    }  
+  }
+
+  save() {
+    this.taskService.updateTasks();
+
+    this.navigateBack();
+  }
+
+  cancel() {
+    this.navigateBack();
+  }
+
+  private navigateBack() {
+    
+    this.router.navigate(['/taskList'], {relativeTo: this.route});
+  }
 }
--- src/app/task-filter/task-filter.ts
+++ src/app/task-filter/task-filter.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-filter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-filter.html',
   styleUrl: './task-filter.css',
 })
 export class TaskFilter {
 
+  filter: string = '';
+
+  @Output() taskFilter = new EventEmitter<string>();
+
+  filterTask() {
+    this.taskFilter.emit(this.filter);
+  }
 }
